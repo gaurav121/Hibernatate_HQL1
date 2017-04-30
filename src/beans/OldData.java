50c37086270c2b7134ed7955f059cbfb5880667c
gstone720 <gyadav@ptc.com>
2017-04-30 22:56:57 +0530

    -a

---
+++ src/beans/OldData.java
@@ -0,0 +1,34 @@
+package beans;
+
+public class OldData {
+
+	private String name;
+	private String email;
+	private long mobileNo;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	
+	
+	
+	
+	
+}
